a7eb87a87929982db1842659343b1679fe73d760
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-03-12 09:14:30 +0700

    adding factory_bot configuration

--- app-template.rb
+++ app-template.rb
@@ -81,6 +81,7 @@ run 'bundle install'
 run 'rails generate rspec:install'
 directory 'spec', 'spec'
 insert_into_file 'spec/rails_helper.rb', after: "require 'rspec/rails'\n" do <<-RUBY
+require 'support/factory_bot'
 require 'devise'
     RUBY
 end
