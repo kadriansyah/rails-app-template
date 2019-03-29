0391bbaebf9294a5e7d3c4061ef5adc3c3a4e406
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-03-29 14:28:59 +0700

    fix bug

--- lib/generators/markazuna/templates/component-html.rb
+++ lib/generators/markazuna/templates/component-html.rb
@@ -1,6 +1,6 @@
 <%%= javascript_pack_tag '<%= @component_path %>/<%= plural_name %>', defer: true %>
 <style>
-    <<%= singular_name %>-list {
+    <%= singular_name %>-list {
         margin: 0;
     }
     .component_container {
@@ -9,6 +9,6 @@
 </style>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12 component_container">
-        <<%= singular_name %>-list data-url="<%= @url %>" form-authenticity-token="<%%= form_authenticity_token.to_s %>"></<%= singular_name %>-page>
+        <<%= singular_name %>-list data-url="<%= @url %>" form-authenticity-token="<%%= form_authenticity_token.to_s %>"></<%= singular_name %>-list>
     </div>
 </div>
